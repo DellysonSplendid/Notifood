8f6b98d003caba045a47eb6fd4a5419059a358ab
Isiaka Lukman Bamidele <isiakalukmandellyson@gmail.com>
2019-07-23 22:07:16 +0100

    updated

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(function(req, res, next) {
 // Morgan
 app.use(morgan("dev"));
 // Routes
-app.use("/", require("./Routes/index"));
+app.use("/", route);
 app.use("/register", userRoute);
 app.use("/login", userRoute);
 app.use("/user/profile", require("./Routes/user"));
